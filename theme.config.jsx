76059117ac2efeb5cc585998081e92e728aa80b2
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-12-16 23:15:46 +0300

    initialize

---
+++ theme.config.jsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router'
+
+export default {
+  logo: <span>Buildog</span>,
+  project: {
+    link: 'https://github.com/MACSEC-Proje-Gelistirme/Community-Portal-Docs'
+  },
+  docsRepositoryBase: 'https://github.com/MACSEC-Proje-Gelistirme/Community-Portal-Docs/tree/main',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Community-Portal'
+      }
+    }
+  },
+  footer: {
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{' '}
+        Community-Portal
+      </span>
+    )
+  },
+  navigation: true, 
+
+  // ... other theme options
+}
